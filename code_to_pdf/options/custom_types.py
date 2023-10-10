d646fba68c7ab8f26321e76c2c3a2717674394e0
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-10-10 22:51:47 +0200

    Define paper size option type with regex

--- code_to_pdf/options/custom_types.py
+++ code_to_pdf/options/custom_types.py
@@ -18,42 +18,9 @@ def is_pygments_style(value: str) -> str:
 PygmentsStyle = Annotated[str, AfterValidator(is_pygments_style)]
 FontSize = Literal["10pt", "11pt", "12pt"]
 Margin = Annotated[str, StringConstraints(pattern=r"^\d+(\.\d+)?in$")]
-
-PaperSize = Literal[
-    "a0paper",
-    "a1paper",
-    "a2paper",
-    "a3paper",
-    "a4paper",
-    "a5paper",
-    "a6paper",
-    "b0paper",
-    "b1paper",
-    "b2paper",
-    "b3paper",
-    "b4paper",
-    "b5paper",
-    "b6paper",
-    "c0paper",
-    "c1paper",
-    "c2paper",
-    "c3paper",
-    "c4paper",
-    "c5paper",
-    "c6paper",
-    "b0j",
-    "b1j",
-    "b2j",
-    "b3j",
-    "b4j",
-    "b5j",
-    "b6j",
-    "ansiapaper",
-    "ansibpaper",
-    "ansicpaper",
-    "ansidpaper",
-    "ansiepaper",
-    "letterpaper",
-    "executivepaper",
-    "legalpaper",
+PaperSize = Annotated[
+    str,
+    StringConstraints(
+        pattern=r"^((([a-c][0-6]|ansi[a-e]|letter|executive|legal)paper)|^(b[0-6]j))$"
+    )
 ]
